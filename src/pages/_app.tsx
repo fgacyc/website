060afd2d665142b2732e0ab4570dc6c7abebd12e
yannisleng <lengzhiyan1015@gmail.com>
2023-09-15 14:59:56 +0800

    Request: Installed NextUI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { NextUIProvider } from '@nextui-org/react'
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  )
 };
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import { Button } from '@nextui-org/react'
 
 export default function Home() {
   return (
@@ -15,7 +16,7 @@ export default function Home() {
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
+            {/* <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
               href="https://create.t3.gg/en/usage/first-steps"
               target="_blank"
@@ -36,7 +37,8 @@ export default function Home() {
                 Learn more about Create T3 App, the libraries it uses, and how
                 to deploy it.
               </div>
-            </Link>
+            </Link> */}
+            <Button>Click me</Button>
           </div>
         </div>
       </main>
