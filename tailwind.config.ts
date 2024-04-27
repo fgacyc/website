91b6a809c1528e8e62f07293b6922419746668c9
Jay Chew <jaychew.3753@gmail.com>
2024-04-27 13:47:47 +0800

    Header main menu view

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import { type Config } from "tailwindcss";
 import {nextui} from "@nextui-org/react";
 
 export default {
-  content: ["./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+  content: ["./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}", "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
