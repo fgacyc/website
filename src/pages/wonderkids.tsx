69aad36eac82f47ba60648e93f5f293bf3a0932f
Jay Chew <jaychew.3753@gmail.com>
2024-05-07 00:02:29 +0800

    resource

--- src/pages/wonderkids.tsx
+++ src/pages/wonderkids.tsx
@@ -12,6 +12,7 @@ import shiningStarImg from "src/assets/wonderkids/agegroups/shining-star.png";
 import superTrooperImg from "src/assets/wonderkids/agegroups/super-trooper.png";
 import wowLifeImg from "src/assets/wonderkids/agegroups/wow-life.png";
 import warriorImg from "src/assets/wonderkids/agegroups/warrior.png";
+import resCardVector from "src/assets/wonderkids/resource_card_vector.svg";
 
 export default function Wonderkids() {
   const ageGroupData = [
@@ -146,7 +147,7 @@ export default function Wonderkids() {
       </section>
       <section>
         <div className={`w-full max-w-[961.7px] mx-auto`}>
-          <h2 className={`text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl font-bold not-italic`}>Age Groups</h2>
+          <h2 className={`text-5xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl font-bold not-italic`}>Age Groups</h2>
           <div className={`mt-12 w-full px-[30px]`}>
             <Slider {...settings}>
               {ageGroupData.map((age_group) => (
@@ -166,6 +167,42 @@ export default function Wonderkids() {
                 </div>
               ))}
             </Slider>
+          </div>
+        </div>
+      </section>
+
+      <section className={`bg-yellow-800`}>
+        <div className={`w-full max-w-[954px] mx-auto`}>
+          <h2 className={`text-5xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl font-bold not-italic`}>Resources</h2>
+          <div className={`mt-12`}>
+            <div className={`inline-block w-fit mr-10`}>
+              <div className={`flex justify-center p-4 max-w-[402px] max-h-[402px] bg-[#FFFFFF] bg-opacity-10 rounded-3xl`}>
+                <div className={`w-[367px] h-[367px] p-10`} style={{backgroundImage: `url(${resCardVector.src})`}}>
+                  <b className={`font-bold text-[33px]`}>Verse Cards</b>
+                  <p className={`text-[25px] mb-12 font-semibold`}>Scripture memorization cards for your kids</p>
+                  <button className={`items-center bg-[#00EDC2] max-w-[300px] w-full rounded-full px-4 py-2 font-bold download-arrow`}>
+                    Download 
+                    <svg className={`inline-block`} width="22" height="12" viewBox="0 0 44 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M43.0607 13.0607C43.6464 12.4749 43.6464 11.5251 43.0607 10.9393L33.5147 1.3934C32.9289 0.807611 31.9792 0.807611 31.3934 1.3934C30.8076 1.97919 30.8076 2.92893 31.3934 3.51472L39.8787 12L31.3934 20.4853C30.8076 21.0711 30.8076 22.0208 31.3934 22.6066C31.9792 23.1924 32.9289 23.1924 33.5147 22.6066L43.0607 13.0607ZM0 13.5H42V10.5H0V13.5Z" fill="black"/>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className={`inline-block w-fit`}>
+              <div className={`flex justify-center p-4 max-w-[402px] max-h-[402px] bg-[#FFFFFF] bg-opacity-10 rounded-3xl`}>
+                <div className={`w-[367px] h-[367px] p-10`} style={{backgroundImage: `url(${resCardVector.src})`}}>
+                  <b className={`font-bold text-[33px]`}>Verse Cards</b>
+                  <p className={`text-[25px] mb-12 font-semibold`}>Scripture memorization cards for your kids</p>
+                  <button className={`items-center bg-[#00EDC2] max-w-[300px] w-full rounded-full px-4 py-2 font-bold download-arrow`}>
+                    Download 
+                    <svg className={`inline-block`} width="22" height="12" viewBox="0 0 44 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M43.0607 13.0607C43.6464 12.4749 43.6464 11.5251 43.0607 10.9393L33.5147 1.3934C32.9289 0.807611 31.9792 0.807611 31.3934 1.3934C30.8076 1.97919 30.8076 2.92893 31.3934 3.51472L39.8787 12L31.3934 20.4853C30.8076 21.0711 30.8076 22.0208 31.3934 22.6066C31.9792 23.1924 32.9289 23.1924 33.5147 22.6066L43.0607 13.0607ZM0 13.5H42V10.5H0V13.5Z" fill="black"/>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
