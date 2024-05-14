b50a7c504a1fa869bfc1cf9c329b76a8d451b091
Jay Chew <jaychew.3753@gmail.com>
2024-05-15 02:50:00 +0800

    Background styling

--- src/pages/wonderkids/index.tsx
+++ src/pages/wonderkids/index.tsx
@@ -6,7 +6,6 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import WkFAQAccordion from "src/components/WkFAQAccordion";
-import Header from "src/layouts/header";
 import wonderkidsBg from "src/assets/wonderkids/BG.png";
 import wonderkidLogo from "src/assets/wonderkids/Logo.png";
 import cradleLandImg from "src/assets/wonderkids/agegroups/cradle-land.png";
@@ -40,7 +39,7 @@ export default function Wonderkids() {
       ages: "0 - 18mo",
       saturday: "7.30PM - 8PM",
       sunday: "2.30PM - 3PM",
-      img: `${cradleLandImg.src}`,
+      img: cradleLandImg,
       css: `bg-[#E05532] text-white`,
     },
     {
@@ -48,7 +47,7 @@ export default function Wonderkids() {
       ages: "19mo - 3",
       saturday: "7.30PM - 8.45PM",
       sunday: "2.30PM - 3.45PM",
-      img: `${shiningStarImg.src}`,
+      img: shiningStarImg,
       css: `bg-[#7E32E0] text-white`,
     },
     {
@@ -56,7 +55,7 @@ export default function Wonderkids() {
       ages: "4 - 6",
       saturday: "5PM - 6.45PM",
       sunday: "2.30PM - 4.30PM",
-      img: `${superTrooperImg.src}`,
+      img: superTrooperImg,
       css: `bg-[#32E0A1] text-white`,
     },
     {
@@ -64,7 +63,7 @@ export default function Wonderkids() {
       ages: "7 - 9",
       saturday: "5PM - 6.45PM",
       sunday: "2.30PM - 4.30PM",
-      img: `${wowLifeImg.src}`,
+      img: wowLifeImg,
       css: `bg-[#E0AF32] text-white`,
     },
     {
@@ -72,7 +71,7 @@ export default function Wonderkids() {
       ages: "10 - 12",
       saturday: "5PM - 6.45PM",
       sunday: "2.30PM - 4.30PM",
-      img: `${warriorImg.src}`,
+      img: warriorImg,
       css: `bg-[#3297E0] text-white`,
     },
   ];
@@ -106,12 +105,12 @@ export default function Wonderkids() {
 
   return (
     <>
-      <main role="main" className={`bg-[#fab800] pt-[68px]`}>
+      <main role="main" className={`bg-[#fab800]`}>
         <section
-          className={"relative z-[0] bg-cover"}
+          className={"relative z-[0] bg-cover bg-no-repeat bg-center h-[810px]"}
           style={{ backgroundImage: `url(${wonderkidsBg.src})` }}
         >
-          <div className={`mx-auto max-w-screen-xl`}>
+          <div className={`mx-auto max-w-[1248px] pt-[68px]`}>
             <svg
               width="9"
               height="189"
@@ -130,13 +129,12 @@ export default function Wonderkids() {
           <Image
             src={`${rocketSrcPath}`}
             alt=""
-            className={`absolute right-0 z-[1]`}
+            className={`max-[929px]:hidden min-[930px]:block min-[930px]:absolute min-[930px]:right-0 min-[930px]:z-[2]`}
             width={`414`}
             height={`430`}
           />
-
           <div
-            className={`relative z-[2] md:ml-[114px] lg:ml-[157px] xl:ml-[200px] 2xl:ml-[243px]`}
+            className={`relative z-[3] md:ml-[151px] lg:ml-[194px] xl:ml-[237px] 2xl:ml-[280px]`}
           >
             <Image
               src={`${wonderkidLogo.src}`}
@@ -234,15 +232,27 @@ export default function Wonderkids() {
               </Link>
             </div>
           </div>
+          {/* Vector 28 */}
+          <svg className={`w-full max-[1267px]:hidden min-[1268px]:block min-[1268px]:absolute min-[1268px]:left-0 min-[1268px]:top-[410px] min-[1268px]:z-[1]`} viewBox="0 0 1440 1748" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1199 286C1295 223.6 1257.67 93.3333 1227 36L1271 0C1411.8 99.2 1444.33 232 1443 286V1748H-9V286C71 226.8 181.667 261.333 227 286C787 450.8 1108.33 354.667 1199 286Z" fill="#1C76C9" />
+          </svg>
+          <div className={`mx-auto mt-12 w-full max-w-[953px] relative z-[3]`}>
+            <img src={`https://placehold.co/953x422`} alt="" width={`953`} height={`422`} />
+          </div>
         </section>
-        <section className={``}>
-          <div className={`mx-auto w-full max-w-[961.7px] pt-24`}>
+
+        <section className={`relative z-[0] h-[1000px]`}>
+          <div className={`mx-auto w-full max-w-[961.7px] relative mt-60`}>
             <h2
-              className={`text-5xl font-bold not-italic sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl`}
+              className={`text-white text-5xl font-bold not-italic sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl`}
             >
               Age Groups
             </h2>
             <div className={`mt-12 w-full px-[30px]`}>
+              {/* Star 6 */}
+              <svg className={`absolute left-[-30px] top-[300px] z-[0]`} width="51" height="51" viewBox="0 0 51 51" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M29.1457 2.76992C31.3195 0.167941 35.5529 1.86778 35.3236 5.25055L34.7642 13.506C34.5738 16.3156 36.2141 18.9282 38.8273 19.9774L46.5058 23.0606C49.6522 24.3239 49.3438 28.8754 46.0557 29.7028L38.0314 31.7217C35.3006 32.4089 33.3227 34.7763 33.1323 37.5858L32.5729 45.8413C32.3436 49.224 27.9196 50.3372 26.1167 47.4657L21.7169 40.4581C20.2195 38.0732 17.3568 36.9237 14.6259 37.6108L6.60163 39.6298C3.31357 40.4571 0.887814 36.5936 3.06162 33.9916L8.36668 27.6417C10.1721 25.4806 10.3807 22.4028 8.88333 20.0179L4.4835 13.0103C2.68062 10.1388 5.60546 6.63787 8.75183 7.90123L16.4304 10.9844C19.0435 12.0337 22.0352 11.2809 23.8406 9.11989L29.1457 2.76992Z" stroke="white" strokeWidth="3" />
+              </svg>
               <Slider {...settings}>
                 {ageGroupData.map((age_group) => (
                   <div
@@ -252,7 +262,7 @@ export default function Wonderkids() {
                     <div
                       className={`flex items-center justify-center pb-[30px] pt-[30px]`}
                     >
-                      <img src={age_group.img} alt="" />
+                      <Image src={age_group.img.src} alt="" width={age_group.img.width} height={age_group.img.height} />
                     </div>
                     <div
                       className={`justtify-center flex flex-col items-center`}
@@ -274,12 +284,22 @@ export default function Wonderkids() {
               </Slider>
             </div>
           </div>
+          {/* Vector 29 */}
+          <svg className={`absolute w-full left-0 z-[1]`} viewBox="0 0 1440 2261" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M338.628 209.678C287.037 1.9308 86.0463 -12.718 -8 5.92594V2158C-8 2158 433 2388 717.5 2158C1002 1928 1443 2158 1443 2158V169.727C1420.43 412.631 1277.75 353.503 1209.23 293.576C1107.12 205.683 856.957 71.4462 673.164 237.644C489.371 403.841 373.559 288.249 338.628 209.678Z" fill="url(#paint0_linear_245_311)" />
+            <defs>
+              <linearGradient id="paint0_linear_245_311" x1="717.5" y1="0" x2="717.5" y2="1588" gradientUnits="userSpaceOnUse">
+                <stop stopColor="#FAB800" />
+                <stop offset="1" stopColor="#FA8700" />
+              </linearGradient>
+            </defs>
+          </svg>
         </section>
 
-        <section className={``}>
-          <div className={`mx-auto w-full max-w-[954px] pt-24`}>
+        <section className={`relative z-[0]`}>
+          <div className={`mx-auto w-full max-w-[954px] pt-24 relative z-[2]`}>
             <h2
-              className={`text-5xl font-bold not-italic sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl`}
+              className={`text-white text-5xl font-bold not-italic sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl`}
             >
               Resources
             </h2>
@@ -357,38 +377,61 @@ export default function Wonderkids() {
                 </div>
               </div>
             </div>
+            {/* Star 3 */}
+            <svg className={`absolute right-[40px] top-[200px] z-[1]`} width="53" height="54" viewBox="0 0 53 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M29.6069 2.34348C32.7124 -1.37362 38.7601 1.05472 38.4326 5.88725L37.8026 15.184C37.6561 17.3452 38.9179 19.3549 40.9281 20.162L49.5751 23.6341C54.0699 25.4389 53.6293 31.941 48.9321 33.1229L39.8956 35.3966C37.795 35.9251 36.2736 37.7462 36.1271 39.9074L35.4971 49.2042C35.1696 54.0367 28.8495 55.6269 26.274 51.5248L21.3192 43.6332C20.1673 41.7987 17.9653 40.9145 15.8646 41.443L6.82813 43.7167C2.13092 44.8986 -1.33446 39.3793 1.77098 35.6621L7.7452 28.5112C9.134 26.8489 9.29444 24.4813 8.14263 22.6468L3.18782 14.7552C0.612274 10.6531 4.79062 5.65179 9.28543 7.45659L17.9325 10.9286C19.9426 11.7358 22.2439 11.1568 23.6327 9.49442L29.6069 2.34348Z" fill="white" />
+            </svg>
           </div>
-        </section>
-
-        <section className={``}>
-          <div className={`mx-auto max-w-[954px] pt-24`}>
+          {/* Star 1 */}
+          <svg className={`absolute left-[80px] top-[500px] z-[1]`} width="62" height="63" viewBox="0 0 62 63" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M35.5287 1.97788C38.6342 -1.73923 44.6819 0.689126 44.3544 5.52166L43.479 18.4388C43.3326 20.5999 44.5944 22.6096 46.6045 23.4168L58.6189 28.2409C63.1138 30.0457 62.6731 36.5478 57.9759 37.7297L45.4205 40.8888C43.3198 41.4173 41.7984 43.2384 41.6519 45.3996L40.7766 58.3167C40.4491 63.1493 34.129 64.7394 31.5535 60.6373L24.6692 49.6726C23.5174 47.8381 21.3153 46.9539 19.2146 47.4825L6.65919 50.6415C1.96198 51.8234 -1.50339 46.3041 1.60205 42.587L9.90274 32.6513C11.2915 30.989 11.452 28.6215 10.3002 26.7869L3.41585 15.8222C0.840312 11.7201 5.01866 6.71881 9.51347 8.52361L21.5279 13.3477C23.538 14.1549 25.8393 13.5759 27.2281 11.9135L35.5287 1.97788Z" fill="white" />
+          </svg>
+          {/* Star 4 */}
+          <svg className={`absolute right-[80px] top-[600px] z-[1]`} width="50" height="52" viewBox="0 0 50 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M28.5422 3.73769C30.716 1.13571 34.9494 2.83556 34.7201 6.21833L34.1607 14.4738C33.9703 17.2833 35.6106 19.8959 38.2238 20.9452L45.9023 24.0284C49.0487 25.2917 48.7403 29.8432 45.4522 30.6705L37.4279 32.6895C34.6971 33.3766 32.7192 35.744 32.5288 38.5536L31.9693 46.809C31.7401 50.1918 27.3161 51.305 25.5132 48.4335L21.1134 41.4258C19.616 39.041 16.7533 37.8915 14.0224 38.5786L5.99811 40.5976C2.71006 41.4249 0.284298 37.5614 2.45811 34.9594L7.76317 28.6094C9.5686 26.4484 9.77718 23.3706 8.27981 20.9857L3.87998 13.978C2.0771 11.1066 5.00195 7.60564 8.14831 8.869L15.8268 11.9522C18.44 13.0014 21.4317 12.2487 23.2371 10.0877L28.5422 3.73769Z" stroke="white" strokeWidth="3" />
+          </svg>
+          {/* Vector 30 */}
+          <svg className={`absolute right-0 top-[500px] z-[1]`} viewBox="0 0 1440 765" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M486 274.058C210.8 344.458 46 154.058 -2 50.0579C92.6667 126.61 310.8 245.784 426 110.058C541.2 -25.6681 850 -7.82794 990 18.0579C1134 43.3912 1414 159.658 1382 422.058C1350 684.458 1454 675.391 1510 638.058V690.058C1434 756.724 1285.2 839.658 1298 638.058C1310.8 436.458 1172.67 340.724 1102 318.058C1011.33 274.058 761.2 203.658 486 274.058Z" fill="white" fillOpacity="0.15" />
+          </svg>
+          {/* Star 5 */}
+          <svg className={`absolute left-0 top-[850px] z-[1]`} width="81" height="107" viewBox="0 0 81 107" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M40.7295 2.81191C42.9033 0.209936 47.1367 1.90978 46.9075 5.29255L44.8923 35.029C44.7019 37.8386 46.3422 40.4512 48.9554 41.5004L76.6137 52.6061C79.7601 53.8694 79.4517 58.4209 76.1636 59.2482L47.2598 66.5207C44.529 67.2079 42.5511 69.5753 42.3607 72.3848L40.3455 102.121C40.1163 105.504 35.6922 106.617 33.8893 103.746L18.041 78.5039C16.5437 76.119 13.6809 74.9696 10.9501 75.6567L-17.9537 82.9292C-21.2418 83.7565 -23.6675 79.8929 -21.4937 77.291L-2.38473 54.4182C-0.579297 52.2571 -0.370717 49.1793 -1.86809 46.7944L-17.7164 21.5526C-19.5193 18.6812 -16.5945 15.1802 -13.4481 16.4436L14.2103 27.5492C16.8234 28.5985 19.8151 27.8458 21.6205 25.6847L40.7295 2.81191Z" stroke="white" strokeWidth="3" />
+          </svg>
+          <div className={`mx-auto max-w-[954px] pt-24 relative z-[2]`}>
             <div className="flex flex-wrap gap-2 py-5 sm:py-8">
               <div className="flex w-auto max-w-[655px] flex-wrap gap-2">
-                <img src={photo1.src} alt="" className="w-full md:w-auto" />
-                <img src={photo2.src} alt="" className="w-full md:w-auto" />
-                <img src={photo4.src} alt="" className="w-full md:w-auto" />
+                <Image src={photo1.src} alt="" className="w-full md:w-auto" width={photo1.width} height={photo1.height} />
+                <Image src={photo2.src} alt="" className="w-full md:w-auto" width={photo2.width} height={photo2.height} />
+                <Image src={photo4.src} alt="" className="w-full md:w-auto" width={photo4.width} height={photo4.height} />
               </div>
               <div className="flex w-auto flex-wrap justify-center gap-2 sm:flex-nowrap lg:max-w-[281px] lg:flex-wrap">
-                <img src={photo3.src} alt="" className="w-full md:w-auto" />
-                <img
+                <Image src={photo3.src} alt="" className="w-full md:w-auto" width={photo3.width} height={photo3.height} />
+                <Image
                   src={photo5.src}
+                  width={photo5.width}
+                  height={photo5.height}
                   alt=""
                   className="w-full sm:object-cover md:w-auto lg:object-none"
                 />
               </div>
               <div className="flex w-auto flex-wrap gap-2">
-                <img src={photo7.src} alt="" className="w-full md:w-auto" />
-                <img src={photo6.src} alt="" className="w-full md:w-auto" />
-                <img src={photo8.src} alt="" className="w-full md:w-auto" />
-                <img src={photo9.src} alt="" className="w-full md:w-auto" />
-                <img src={photo10.src} alt="" className="w-full md:w-auto" />
+                <Image src={photo7.src} width={photo7.width} height={photo7.height} alt="" className="w-full md:w-auto" />
+                <Image src={photo6.src} width={photo6.width} height={photo6.height} alt="" className="w-full md:w-auto" />
+                <Image src={photo8.src} width={photo8.width} height={photo8.height} alt="" className="w-full md:w-auto" />
+                <Image src={photo9.src} width={photo9.width} height={photo9.height} alt="" className="w-full md:w-auto" />
+                <Image src={photo10.src} width={photo10.width} height={photo10.height} alt="" className="w-full md:w-auto" />
               </div>
             </div>
           </div>
+          {/* Star 2 */}
+          <svg className={`absolute right-[27px] top-[1000px] z-[1]`} width="84" height="86" viewBox="0 0 84 86" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M49.8088 2.44749C52.9143 -1.26962 58.962 1.15873 58.6345 5.99126L57.1997 27.1627C57.0533 29.3239 58.3151 31.3336 60.3252 32.1407L80.0171 40.0476C84.5119 41.8524 84.0713 48.3546 79.3741 49.5364L58.7954 54.7142C56.6948 55.2428 55.1734 57.0639 55.0269 59.225L53.5922 80.3965C53.2647 85.2291 46.9446 86.8193 44.3691 82.7172L33.0855 64.7457C31.9337 62.9112 29.7316 62.027 27.631 62.5556L7.05231 67.7334C2.35509 68.9152 -1.11028 63.3959 1.99516 59.6788L15.6002 43.394C16.989 41.7317 17.1494 39.3642 15.9976 37.5296L4.71408 19.5582C2.13853 15.4561 6.31688 10.4548 10.8117 12.2596L30.5036 20.1665C32.5137 20.9736 34.815 20.3946 36.2038 18.7322L49.8088 2.44749Z" fill="white" />
+          </svg>
         </section>
 
-        <section className={``}>
-          <div className={`mx-auto max-w-[955px] pb-24 pt-24`}>
+        <section className={`relative z-[0]`}>
+          <div className={`mx-auto max-w-[955px] pb-56 pt-60 relative z-[1]`}>
             <div className="flex flex-col lg:flex-row">
               <div className="w-full lg:w-4/12">
                 <div className="text-8xl font-bold text-white max-lg:text-center lg:text-9xl">
