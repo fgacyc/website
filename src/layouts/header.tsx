6558ad752ac4be2b452e1ef57070fd16815fd1b5
Jay Chew <jaychew.3753@gmail.com>
2024-05-23 20:11:55 +0800

    Change of Header

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
-
 export default function Header() {
   const router = useRouter();
   const getHeaderMainMenuCSS = () => {
