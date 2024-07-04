89ec773382195e6952e9c658eaee58580be9b2cb
Innis <1575270674@qq.com>
2024-07-04 21:00:05 +0800

    feat: add get-to-know-people page

---
+++ src/pages/pray.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PrayerRequest from "~/pages/fga-mobile/prayer-request";
+
+const Pray: React.FC = () => {
+    return (
+       <>
+        <PrayerRequest />
+       </>
+    );
+}
+
+export default Pray;
