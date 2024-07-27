c59ed799861a2fd2554522164939997091ed9fe5
Innis <1575270674@qq.com>
2024-07-27 11:31:15 +0800

    fix: blogs style

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -18,8 +18,8 @@ const BlogCard = ({blog}: { blog: Blog }) => {
         <div className={"border m-4 rounded cursor-pointer"}
                 onClick={handleClick}
         >
-            <img src={blog.image_url} alt={blog.title} className={"w-[300px]"}/>
-            <div className={"text-lg font-bold"}>{blog.title}</div>
+            <img src={blog.image_url} alt={blog.title} className={""}/>
+            <div className={"text-lg font-bold p-4 "}>{blog.title}</div>
         </div>
     );
 }
@@ -30,7 +30,7 @@ const BlogPage = ({allBlogData}: { allBlogData: Blog[] }) => {
         <div>
             <div className={"h-16"}></div>
             {/*<h1>Blog</h1>*/}
-            <div className={`grid grid-cols-3 gap-4 `}>
+            <div className={`grid lg:grid-cols-4 sm:grid-cols-3 grid-cols-1 gap-4 `}>
                 {allBlogData.map((blog,index) => (
                     <BlogCard key={index} blog={blog} />
                 ))}
