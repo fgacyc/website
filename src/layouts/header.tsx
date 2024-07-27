64ac37f8ecc86bf9e63e5539640ca7cf5ab568ed
Innis <1575270674@qq.com>
2024-07-27 16:08:55 +0800

    feat: add  blog

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -158,6 +158,11 @@ export default function Header() {
                     className="block px-4 py-2 pr-6 text-right text-3xl text-black hover:bg-[#00EDC2] lg:text-4xl xl:text-5xl" >
                     Stream
                 </Link>
+                <Link href="/blog"
+                      target="_blank"
+                      className="block px-4 py-2 pr-6 text-right text-3xl text-black hover:bg-[#00EDC2] lg:text-4xl xl:text-5xl" >
+                    Blog
+                </Link>
                 <Link href="/mobile-app-download"
                     className="block px-4 py-2 pr-6 text-right text-3xl text-black hover:bg-[#00EDC2] lg:text-4xl xl:text-5xl" >
                     Mobile App
