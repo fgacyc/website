33d555c6a5ec82a332ae45ed1d0020d74aa88ac7
Innis <1575270674@qq.com>
2024-07-29 17:21:58 +0800

    feat: add The Open Graph info

--- src/pages/components/layout.tsx
+++ src/pages/components/layout.tsx
@@ -1,9 +1,20 @@
 import Header from "~/layouts/header";
 import Footer from "~/layouts/footer";
+import Head from "next/head";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-w-screen relative">
+        {/* eslint-disable-next-line react/jsx-no-undef */}
+        <Head>
+            <link rel="icon" href="/CYC_Logo.jpg"/>
+            <meta property="og:title" content="FGA Technology"/>
+            <meta property="og:description"
+                  content="100 Strong Churches, 100,000 Disciples."/>
+            <meta property="og:image" content="https://www.fgacyc.tech/CYC_Logo.jpg"/>
+            <meta property="og:url" content="https://www.fgacyc.com"/>
+            <meta property="og:type" content="website"/>
+        </Head>
       <Header />
       <main>{children}</main>
       <Footer />
