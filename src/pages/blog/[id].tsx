ed72e8f364c8bb1f8d7d85eef740ff4bab8be706
ETCasual <casualorient@gmail.com>
2024-07-31 02:31:58 +0800

    fix: remove sole api helpers from api folders to src/helpers

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -1,44 +1,47 @@
-import {type GetStaticPaths, type GetStaticProps} from "next";
-import {type Blog, getBlog, getBlogs} from "~/pages/api/blog";
+import { type GetStaticPaths, type GetStaticProps } from "next";
+import { type Blog, getBlog, getBlogs } from "~/helpers";
 import MdViewer from "~/components/md-viewer";
 
-const BlogPage = ({blogData}: { blogData: Blog }) => {
-    const {title, content,image_url} = blogData;
-    return (
-        <div>
-            <div className={"h-16"}></div>
-            <img src={image_url} alt={title} className={"w-full"}/>
-            <div className={`sm:p-8 p-4  max-w-[1000px]  mx-auto
+const BlogPage = ({ blogData }: { blogData: Blog }) => {
+  const { title, content, image_url } = blogData;
+  return (
+    <div>
+      <div className={"h-16"}></div>
+      <img src={image_url} alt={title} className={"w-full"} />
+      <div
+        className={`mx-auto max-w-[1000px]  p-4  sm:p-8
             
-            `}>
-                <div className={"sm:text-5xl text-2xl text-center font-bold mb-8"}>{title}</div>
-                <MdViewer content={content}/>
-            </div>
+            `}
+      >
+        <div className={"mb-8 text-center text-2xl font-bold sm:text-5xl"}>
+          {title}
         </div>
-    );
+        <MdViewer content={content} />
+      </div>
+    </div>
+  );
 };
 export default BlogPage;
 
-
 export const getStaticPaths: GetStaticPaths = async () => {
-    // 这里你需要返回所有的博客 id
-    const blogs = await getBlogs(); // 假设你有一个方法来获取所有博客
-    const paths = blogs.map(blog => ({
-        params: {id: blog.id.toString()},
-    }));
-    return {paths, fallback: false};
+  // 这里你需要返回所有的博客 id
+  const blogs = await getBlogs(); // 假设你有一个方法来获取所有博客
+  const paths = blogs.map((blog) => ({
+    params: { id: blog.id.toString() },
+  }));
+  return { paths, fallback: false };
 };
 
-export const getStaticProps: GetStaticProps = async ({params}) => {
-    const {id} = params!;
-    if (typeof id !== 'string') {
-        throw new Error('Invalid id');
-    }
-    const blogData: Blog = await getBlog(id);
-    return {
-        props: {
-            blogData,
-        },
-        revalidate: 60,
-    };
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { id } = params!;
+  if (typeof id !== "string") {
+    throw new Error("Invalid id");
+  }
+  const blogData: Blog = await getBlog(id);
+  return {
+    props: {
+      blogData,
+    },
+    revalidate: 60,
+  };
 };
