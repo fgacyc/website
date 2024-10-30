f1a1ffd682458b7bcbf2fc06621cc534206a82c4
Jia Hao Chin <jiahao.c@RUSHs-MBP.realtek>
2024-10-30 22:57:42 +0800

    Add redirect to the blessing conf

---
+++ src/pages/theblessing.tsx
@@ -0,0 +1,10 @@
+import { useEffect } from 'react'
+
+const TheBlessingPage = () => {
+    useEffect(()=> {
+        window.open("https://theblessing.fgacyc.com/","_self")
+    },[])
+    return <></>
+}
+
+export default TheBlessingPage
