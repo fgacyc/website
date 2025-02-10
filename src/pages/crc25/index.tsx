1e90dd2ba7eb7998469cfd0f6fc6894af7fc9946
Innis <1575270674@qq.com>
2025-02-10 22:43:18 +0800

    feat: add crc25 redirect text

--- src/pages/crc25/index.tsx
+++ src/pages/crc25/index.tsx
@@ -1,12 +1,10 @@
 import {useEffect} from "react";
+import Loading from "~/pages/components/loading";
 
 export default function CRC25() {
   // redirect to crc25.fgacyc.com
     useEffect(() => {
         window.location.href = "https://crc25.fgacyc.com";
     }, []);
-    return (
-        <>
-        </>
-    );
+    return <Loading page_title={"CRC25"} />;
 }
