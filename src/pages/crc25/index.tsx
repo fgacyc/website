4a6d8c7c58f740316a9fedf347572e99949d205b
Innis <1575270674@qq.com>
2025-02-10 22:20:36 +0800

    feat: add crc25 useeffect

--- src/pages/crc25/index.tsx
+++ src/pages/crc25/index.tsx
@@ -1,6 +1,10 @@
+import {useEffect} from "react";
+
 export default function CRC25() {
   // redirect to crc25.fgacyc.com
-    window.location.href = "https://crc25.fgacyc.com";
+    useEffect(() => {
+        window.location.href = "https://crc25.fgacyc.com";
+    }, []);
     return (
         <>
         </>
