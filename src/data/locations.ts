62b89bbd79210c352021567e5da8d3dd2b878442
Innis <1575270674@qq.com>
2025-09-22 21:07:38 +0800

    fix: GA to Genrral Service, WD to Wonderkids

--- src/data/locations.ts
+++ src/data/locations.ts
@@ -2,37 +2,37 @@ export const locations = [
   {
     name: "Kuchai Lama",
     addr: "Lot 689, Jalan Kuchai Lama, Taman Goodwood, 58200 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur",
-    services: ["SATURDAY 3PM • 5.30PM", "SUNDAY 1.30PM"],
+    services: ["SATURDAY 3PM (Young Warrior)","SATURDAY 5:30PM (Genrral Service)", "SUNDAY 1:30PM (Genrral Service)"],
   },
   {
     name: "Setapak",
     addr: "2nd Floor, Lot 9163, Prima Setapak, Batu 4, Jalan Genting Kelang, 53300 Kuala Lumpur",
-    services: ["SATURDAY 5PM (Youth)", "SUNDAY 10.30AM (GS)"],
+    services: ["SATURDAY 5PM (Youth)", "SUNDAY 10:30AM (Genrral Service)"],
   },
   {
     name: "Kepong",
     addr: "Full Gospel Assembly Bhd Sg Mas Plaza. Lot 10B, Jalan Ipoh, Batu 5, 51200 Kuala Lumpur, WP Kuala Lumpur",
-    services: ["SATURDAY 7.30PM (GS)", "SUNDAY 10.30AM (GS)"],
+    services: ["SATURDAY 7:30PM (Genrral Service)", "SUNDAY 10:30AM (Genrral Service)"],
   },
   {
     name: "Sg. Long",
     addr: "Mahkota Point, Level 1, Lot 35052, Persiaran Mahkota Cheras 1, Jalan Penggwa, Bandar Mahkota Cheras, 43200 Cheras, Selangor",
-    services: ["SATURDAY 7:30PM (Chinese)","SUNDAY 10:30am (English & WK)", "SUNDAY 1:30pm (Bilingual & WK)"],
+    services: ["SATURDAY 7:30PM (Chinese)","SUNDAY 10:30am (English & Wonderkids)", "SUNDAY 1:30pm (Bilingual & Wonderkids)"],
   },
   {
     name: "Seremban",
     addr: "Blok A14-2 & A15-2, Block A, Jalan Haruan 5/9, Pusat Komersial Oakland 2, 70300 Seremban, Negeri Sembilan",
-    services: ["SUNDAY 10.30AM (GS & WK)"],
+    services: ["SUNDAY 10:30AM (Genrral Service & Wonderkids)"],
   },
   {
     name: "USJ",
     addr: "12 & 14, Jalan Indusri USJ 1/6, Taman Perindustrian USJ 1,47600 Subang Jaya, Selangor Darul Ehsan, Malaysia",
-    services: ["SATURDAY 4.30PM (Young Warrior)", "SUNDAY 10.30AM (General Service & Wonderkids)"],
+    services: ["SATURDAY 4:30PM (Young Warrior)", "SUNDAY 10:30AM (General Service & Wonderkids)"],
   },
   {
     name: "Serdang",
     addr: "24 & 26, Jalan PSK 4, Pusat Perdagangan Seri Kembangan, 43300 Seri Kembangan, Selangor",
-    services: ["SUNDAY 10.30AM (GS & WK)", "SUNDAY 1.30PM (GS)"],
+    services: ["SUNDAY 10:30AM (General Service & Wonderkids)", "SUNDAY 1:30PM (General Service)"],
   },
   // {
   //   name: "The Blessing (Puchong)",
