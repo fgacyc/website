dbbf269c7d3c793c7263c3d9c90c61611df75b0c
Innis <1575270674@qq.com>
2025-11-02 16:50:46 +0800

    feat: add crossover event

---
+++ src/pages/crossover.tsx
@@ -0,0 +1,13 @@
+import { type FC, useEffect } from "react";
+import Loading from "~/pages/components/loading";
+
+const CrossoverEvent: FC = () => {
+    useEffect(() => {
+        window.location.href =
+            "https://vbgif.my.canva.site/crossover-2025-landing-page";
+    }, []);
+
+    return <Loading page_title={"Itunes"} />;
+};
+
+export default CrossoverEvent;
